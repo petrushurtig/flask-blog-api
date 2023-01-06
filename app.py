5c4f97093ca2429c6ab90eb92e6bb6585398bde6
Petrus Hurtig <petrushurtig@hotmail.com>
2023-01-06 21:23:18 +0200

    env, gitignore, db init

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
+import os
+from dotenv import load_dotenv
+from flask_migrate import Migrate
+
+from src.db.config.db import db
 from app_source import app
+
+
+load_dotenv()
+
+app.config["SQLALCHEMY_DATABASE_URI"]=os.environ["DATABASE_URL"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+migrate = Migrate(app, db, compare_type=True)
+
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
